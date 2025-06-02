b0758a200351fc921be3913184ee057e64a6401a
Islam Sulaiman <islam.sulaiman@procore.com>
2025-06-02 12:38:01 +0300

    fix: nvim - fix dot character indents wrongly to the left in ruby code

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -42,12 +42,22 @@ return { -- Highlight, edit, and navigate code
       --  the list of additional_vim_regex_highlighting and disabled languages for indent.
       additional_vim_regex_highlighting = { 'ruby' },
     },
+    -- Keep Ruby disabled for treesitter indent as originally intended
     indent = { enable = true, disable = { 'ruby' } },
   },
-  -- There are additional nvim-treesitter modules that you can use to interact
-  -- with nvim-treesitter. You should go explore a few and see what interests you:
-  --
-  --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-  --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-  --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+
+  -- To prevent dot `.` from indenting the code due to a conflict
+  config = function(_, opts)
+    require('nvim-treesitter.configs').setup(opts)
+    -- Fix Ruby dot key indentation issue
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "ruby",
+      callback = function()
+        -- Remove the dot from indentkeys to prevent auto-indentation on dot
+        vim.bo.indentkeys = vim.bo.indentkeys:gsub(",?%.","")
+        -- Use Vim's built-in Ruby indentation instead of treesitter
+        vim.bo.indentexpr = "GetRubyIndent()"
+      end,
+    })
+  end,
 }
--- nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -1,9 +1,10 @@
 return {
   "christoomey/vim-tmux-navigator",
   commit = "791dacfcfc8ccb7f6eb1c853050883b03e5a22fe",
-  vim.keymap.set('n', 'C-h', ':TmuxNavigateLeft<CR>'),
-  vim.keymap.set('n', 'C-j', ':TmuxNavigateDown<CR>'),
-  vim.keymap.set('n', 'C-k', ':TmuxNavigateUp<CR>'),
-  vim.keymap.set('n', 'C-l', ':TmuxNavigateRight<CR>'),
-
+  config = function()
+    vim.keymap.set('n', '<C-h>', ':TmuxNavigateLeft<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<C-j>', ':TmuxNavigateDown<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<C-k>', ':TmuxNavigateUp<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<C-l>', ':TmuxNavigateRight<CR>', { noremap = true, silent = true })
+  end,
 }
