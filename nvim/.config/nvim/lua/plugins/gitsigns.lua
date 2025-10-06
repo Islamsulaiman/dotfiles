fbb2403cecc5833ee9c0874c0707dace9294e1c0
Islam Sulaiman <islam.sulaiman@procore.com>
2025-10-06 09:35:38 +0300

    feat: nvim-add keymaps for looping through changes in the buffer

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,16 +1,35 @@
--- Adds git related signs to the gutter, as well as utilities for managing changes
 -- lua/plugins/gitsigns.lua
 return {
   'lewis6991/gitsigns.nvim',
-  version = "*", -- Use latest stable release
   event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    require('gitsigns').setup {
-      current_line_blame = true, -- Toggle with <leader>gb
-    }
+  opts = {
+    current_line_blame = true, -- Toggle with <leader>gb
 
-    vim.keymap.set('n', '<leader>gp', ':Gitsigns preview_hunk<CR>', {})
-    vim.keymap.set('n', '<leader>gb', ':Gitsigns toggle_current_line_blame<CR>', {})
-    vim.keymap.set('n', '<leader>gr', ':Gitsigns reset_hunk<CR>', {})
-  end,
+    -- This function runs when gitsigns attaches to a buffer.
+    on_attach = function(bufnr)
+      -- We no longer define 'gs' here.
+
+      -- Navigation
+      vim.keymap.set('n', ']c', function()
+        if vim.wo.diff then return ']c' end
+        -- Use require('gitsigns') directly inside the scheduled function
+        vim.schedule(function() require('gitsigns').next_hunk() end)
+        return '<Ignore>'
+      end, { expr = true, buffer = bufnr, desc = "Next Git Hunk" })
+
+      vim.keymap.set('n', '[c', function()
+        if vim.wo.diff then return '[c' end
+        -- Use require('gitsigns') directly inside the scheduled function
+        vim.schedule(function() require('gitsigns').prev_hunk() end)
+        return '<Ignore>'
+      end, { expr = true, buffer = bufnr, desc = "Previous Git Hunk" })
+
+      -- Actions
+      -- It's good practice to get the module here too for consistency.
+      local gs = require('gitsigns')
+      vim.keymap.set('n', '<leader>gp', gs.preview_hunk, { buffer = bufnr, desc = "Preview Git Hunk" })
+      vim.keymap.set('n', '<leader>gb', gs.toggle_current_line_blame, { buffer = bufnr, desc = "Toggle Git Blame" })
+      vim.keymap.set('n', '<leader>gr', gs.reset_hunk, { buffer = bufnr, desc = "Reset Git Hunk" })
+    end,
+  },
 }
